9dea25163ceaf80c8b6b91472dd904a0214dcb01
Derek Smith <derek.rians.smith@gmail.com>
2014-05-18 06:03:38 -0700

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-oscar-stripe',
-      version='0.1',
+      version='0.2',
       url='https://github.com/tangentlabs/django-oscar-stripe',
       author="David Winterbottom",
       author_email="david.winterbottom@tangentlabs.co.uk",
